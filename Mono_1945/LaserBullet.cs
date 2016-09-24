b3ee61badf412fd8d9958e622a965baed6062636
skadush <nelzkie.symbian@gmail.com>
2016-09-24 18:25:01 +1200

    initial commit

---
+++ Mono_1945/LaserBullet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Sprites;
+
+namespace Mono_1945
+{
+    
+    public class LaserBullet
+    {
+        Sprite bullet;
+
+        public LaserBullet(Texture2D texture)
+        {
+            bullet = new Sprite(texture);
+        }
+
+
+        public void Update()
+        {
+            
+        }
+    }
+}
